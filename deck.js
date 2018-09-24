8d51029e7b83fe378e8b3c70151aad952b90ebcc
Dustin Schau <dustinschau@gmail.com>
2018-09-24 13:45:57 -0500

    feat: initial commit

---
+++ deck.js
@@ -0,0 +1,13 @@
+import slides from './slides.mdx'
+
+// import './fonts/Webfonts/futurapt_book_macroman/stylesheet.css'
+// import './fonts/Webfonts/futurapt_bookitalic_macroman/stylesheet.css'
+// import './fonts/Webfonts/futurapt_demi_macroman/stylesheet.css'
+// import './fonts/Webfonts/futurapt_demiitalic_macroman/stylesheet.css'
+// import 'typeface-space-mono'
+// import 'typeface-spectral'
+
+export { default as theme } from 'gatsby-mdx-theme'
+export default [
+  ...slides
+]
